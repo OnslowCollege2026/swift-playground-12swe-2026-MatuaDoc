cee1471ce9b91f139b4bd2faff42986a408b6be4
Jaesan Ryfle-Turi <88599935+MatuaDoc@users.noreply.github.com>
2026-04-23 02:20:29 +0000

    Apr 23, 2026, 2:20 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,6 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        
+        let madLibString = "Once upon a time, there was a \(adjective1) \(noun1) who loved to \(verb1) every day."
     }
 }
