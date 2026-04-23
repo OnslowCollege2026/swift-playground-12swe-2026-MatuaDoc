422d9131dc38acd744acbad11b67eaf3521a344f
Jaesan Ryfle-Turi <88599935+MatuaDoc@users.noreply.github.com>
2026-04-23 02:45:05 +0000

    Apr 23, 2026, 2:45 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -6,7 +6,7 @@ struct SwiftPlayground {
     static func main() {
         let adjective1 = readLine()!
         let noun1 = readLine()!
-        let verb1 = "sing"
+        let verb1 = readLine()!
 
         let madLibString = "Once upon a time, there was a \(adjective1) \(noun1) who loved to \(verb1) every day."
 
